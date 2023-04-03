9b7b3b1caf7d529dfff999ce5b11986442d53d34
enioaiello <eaiello@edenschool.fr>
2023-04-03 14:21:47 +0200

    Update website

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-let denied = document.querySelector(".denied");
-let errorBox = document.querySelector("#unavailableDiv");
-denied.addEventListener("click", notAllowed);
-function notAllowed() {
-    errorBox.classList.remove("none");
-    setTimeout(() => {
-        errorBox.classList.add("none");
-    }, 5000);
-}
